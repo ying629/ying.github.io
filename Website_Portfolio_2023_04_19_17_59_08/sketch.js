83a863756046d98797363698f71510eebb8c65bb
ying629 <yingchu06@gmail.com>
2023-04-19 14:00:09 -0400

    P5js documents

---
+++ Website_Portfolio_2023_04_19_17_59_08/sketch.js
@@ -0,0 +1,29 @@
+//stable pastel colors
+//mouse over "pulse" and changes colors
+
+
+var canvas;
+
+function setup() {
+	canvas = createCanvas(window.innerWidth, window.innerHeight);
+	// canvas.position(0,0);
+    strokeWeight(0);
+}
+
+function draw() {
+	clear();
+	background(color('white'));
+}
+
+
+
+// https://p5js.org/reference/#/p5/image
+// https://stackoverflow.com/questions/39126139/is-there-a-way-to-load-and-clip-an-image-with-a-round-border-in-p5-js
+
+
+
+
+
+
+
+
